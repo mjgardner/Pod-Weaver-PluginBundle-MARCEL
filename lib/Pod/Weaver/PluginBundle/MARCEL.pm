d386d26484faca3ba731b726c963ccaa4d61e021
Marcel Gruenauer <hanekomu@gmail.com>
2010-03-29 19:18:51 +0200

    methods are now collected with [CollectWithAutoDoc]

--- lib/Pod/Weaver/PluginBundle/MARCEL.pm
+++ lib/Pod/Weaver/PluginBundle/MARCEL.pm
@@ -3,40 +3,38 @@ use strict;
 use warnings;
 
 package Pod::Weaver::PluginBundle::MARCEL;
+
 # ABSTRACT: build POD documentation like MARCEL
-
 use namespace::autoclean;
-
 use Pod::Weaver::Config::Assembler;
+
+# plugins used
+use Pod::Weaver::Section::Installation;
+use Pod::Weaver::Section::CollectWithAutoDoc;
+
 sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
 
 sub mvp_bundle_config {
-  return (
-    [ '@Default/CorePrep',  _exp('@CorePrep'), {} ],
-    [ '@Default/Name',      _exp('Name'),      {} ],
-    [ '@Default/Version',   _exp('Version'),   {} ],
-
-    [ '@Default/prelude',   _exp('Region'),    { region_name => 'prelude'  } ],
-    [ 'SYNOPSIS',           _exp('Generic'),   {} ],
-    [ 'DESCRIPTION',        _exp('Generic'),   {} ],
-    [ 'OVERVIEW',           _exp('Generic'),   {} ],
-
-    [ 'ATTRIBUTES',         _exp('Collect'),   { command => 'attr'     } ],
-    [ 'METHODS',            _exp('Collect'),   { command => 'method'   } ],
-    [ 'FUNCTIONS',          _exp('Collect'),   { command => 'function' } ],
-
-    [ '@Default/Leftovers', _exp('Leftovers'), {} ],
-
-    [ '@Default/postlude',  _exp('Region'),    { region_name => 'postlude' } ],
-
-    [ '@Default/Installation',       _exp('Installation'),       {} ],
-    [ '@Default/BugsAndLimitations', _exp('BugsAndLimitations'), {} ],
-    [ '@Default/Availability',       _exp('Availability'),       {} ],
-    [ '@Default/Authors',            _exp('Authors'),            {} ],
-    [ '@Default/Legal',              _exp('Legal'),              {} ],
-  )
+    return (
+        [ '@Default/CorePrep', _exp('@CorePrep'), {} ],
+        [ '@Default/prelude', _exp('Region'),  { region_name => 'prelude' } ],
+        [ '@Default/Name',    _exp('Name'),    {} ],
+        [ '@Default/Version', _exp('Version'), {} ],
+        [ 'SYNOPSIS',         _exp('Generic'), {} ],
+        [ 'DESCRIPTION',      _exp('Generic'), {} ],
+        [ 'OVERVIEW',         _exp('Generic'), {} ],
+        [ 'ATTRIBUTES',       _exp('Collect'), { command     => 'attr' } ],
+        [ 'METHODS',   _exp('CollectWithAutoDoc'), { command => 'method' } ],
+        [ 'FUNCTIONS', _exp('Collect'),            { command => 'function' } ],
+        [ '@Default/Leftovers', _exp('Leftovers'), {} ],
+        [ '@Default/postlude', _exp('Region'), { region_name => 'postlude' } ],
+        [ '@Default/Installation',       _exp('Installation'),       {} ],
+        [ '@Default/BugsAndLimitations', _exp('BugsAndLimitations'), {} ],
+        [ '@Default/Availability',       _exp('Availability'),       {} ],
+        [ '@Default/Authors',            _exp('Authors'),            {} ],
+        [ '@Default/Legal',              _exp('Legal'),              {} ],
+    );
 }
-
 1;
 
 =pod
@@ -70,7 +68,7 @@ equivalent to the following:
     [Collect / ATTRIBUTES]
     command = attr
 
-    [Collect / METHODS]
+    [CollectWithAutoDoc / METHODS]
     command = method
 
     [Collect / FUNCTIONS]
