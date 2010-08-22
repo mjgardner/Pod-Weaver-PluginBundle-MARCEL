88d9e43e59d3d31938087ec473c2b5e6258a9dd3
Nigel Metheringham <nigelm@cpan.org>
2010-08-22 17:25:03 +0100

    Made Availability plugin use dist metadata info
    Availability weaver plug was using a hardwired github base
    URL.  Added more flexibility so that any repository URL and
    repository type (defaults to git) will be picked up from the
    dist metadata.
    
    If the home page URL is not the CPAN default then an additional
    paragraph with this information is added in.
    
    If the repository is on github some additional magic is added,
    otherwise a minimal generic paragraph is added.  This could later
    be extended for other repositories.

--- lib/Pod/Weaver/Section/Availability.pm
+++ lib/Pod/Weaver/Section/Availability.pm
@@ -10,33 +10,128 @@ with 'Pod::Weaver::Role::Section';
 use namespace::autoclean;
 use Moose::Autobox;
 
+# add a set of attributes to hold the repo information
+has zilla => ( is => 'rw', isa => 'Dist::Zilla', handles => [ 'name', 'distmeta' ] );
+has homepage_url => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_homepage_url' );
+has cpan_url     => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_cpan_url' );
+has repo_type    => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_repo_type' );
+has repo_url     => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_repo_url' );
+has repo_web     => ( is => 'rw', isa => 'Str',  lazy => 1, builder => '_build_repo_web' );
+has is_github    => ( is => 'rw', isa => 'Bool', lazy => 1, builder => '_build_is_github' );
+
 sub weave_section {
-    my ($self, $document, $input) = @_;
-    my $name = $input->{zilla}->name;
+    my ( $self, $document, $input ) = @_;
+
+    $self->zilla( $input->{zilla} );
     $document->children->push(
         Pod::Elemental::Element::Nested->new(
-            {   command  => 'head1',
+            {
+                command  => 'head1',
                 content  => 'AVAILABILITY',
-                children => [
-                    Pod::Elemental::Element::Pod5::Ordinary->new(
-                        {   content => <<EOPOD,
-The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you, or see
-L<http://search.cpan.org/dist/$name/>.
-
-The development version lives at
-L<http://github.com/hanekomu/$name/>.
-Instead of sending patches, please fork this project using the standard git
-and github infrastructure.
-EOPOD
-                        }
-                    ),
-                ],
+                children => [ $self->_homepage_pod, $self->_cpan_pod, $self->_development_pod, ],
             }
         ),
     );
 }
+
+sub _build_homepage_url {
+    my $self = shift;
+
+    return $self->distmeta->{resources}{homepage} || sprintf( 'http://search.cpan.org/dist/%s/', $self->name );
+}
+
+sub _build_cpan_url {
+    my $self = shift;
+
+    return sprintf( 'http://search.cpan.org/dist/%s/', $self->name );
+}
+
+sub _build_repo_type {
+    my $self = shift;
+
+    # if we don't know we default to git...
+    return $self->distmeta->{resources}{repository}{type} || 'git';
+}
+
+sub _build_repo_url {
+    my $self = shift;
+
+    return ( $self->_build_repo_data )[0];
+}
+
+sub _build_repo_web {
+    my $self = shift;
+
+    return ( $self->_build_repo_data )[1];
+}
+
+sub _build_is_github {
+    my $self = shift;
+
+    # we do this by looking at the URL for githubbyness
+    my $repourl = $self->distmeta->{resources}{repository}{url} or die "No repository URL set in distmeta";
+    return ( ( $repourl =~ m|/github.com/| ) ? 1 : 0 );
+}
+
+sub _build_repo_data {
+    my $self = shift;
+
+    my $repourl = $self->distmeta->{resources}{repository}{url} or die "No repository URL set in distmeta";
+    my $repoweb;
+    if ( $self->is_github ) {
+
+        # strip the access method off - we can then add it as needed
+        my $nomethod = $repourl;
+        $nomethod =~ s!^(http|git|git\@github\.com):/*!!i;
+        $repourl = 'git://' . $nomethod;
+        $repoweb = 'http://' . $nomethod;
+    }
+    return ( $repourl, $repoweb );
+}
+
+sub _homepage_pod {
+    my $self = shift;
+
+    # we suppress this if the CPAN URL is the homepage URL
+    return if ( $self->cpan_url eq $self->homepage_url );
+
+    # otherwise return some boilerplate
+    return Pod::Elemental::Element::Pod5::Ordinary->new(
+        { content => sprintf( 'The project homepage is L<%s>.', $self->homepage_url ) } );
+}
+
+sub _cpan_pod {
+    my $self = shift;
+
+    my $text = sprintf(
+        "%s\n%s\n%s L<%s>.",
+        'The latest version of this module is available from the Comprehensive Perl',
+        'Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN',
+        'site near you, or see',
+        $self->cpan_url
+    );
+
+    return Pod::Elemental::Element::Pod5::Ordinary->new( { content => $text } );
+}
+
+sub _development_pod {
+    my $self = shift;
+
+    my $text;
+    if ( $self->is_github ) {
+        $text = sprintf( "The development version lives at L<%s>\n", $self->repo_web );
+        $text .= sprintf( "and may be cloned from L<%s>.\n", $self->repo_url );
+        $text .= "Instead of sending patches, please fork this project using the standard\n";
+        $text .= "git and github infrastructure.\n"
+
+    }
+    else {
+        $text = sprintf( "The development version lives in a %s repository at L<%s>\n", $self->repo_type, $self->repo_web );
+    }
+
+    return Pod::Elemental::Element::Pod5::Ordinary->new( { content => $text } );
+}
+
 1;
 
 =pod
